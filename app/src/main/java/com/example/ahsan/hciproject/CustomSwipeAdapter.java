b7f168f0f2c16b3d7fee9ff4705857b55c4f82e1
sam17896 <ahsan.kai@gmail.com>
2017-11-10 16:50:29 +0500

    slider and flip animation

---
+++ app/src/main/java/com/example/ahsan/hciproject/CustomSwipeAdapter.java
@@ -0,0 +1,65 @@
+package com.example.ahsan.hciproject;
+
+import android.content.Context;
+import android.media.Image;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextClock;
+import android.widget.TextView;
+
+/**
+ * Created by Sohail Arshad on 11/10/2017.
+ */
+
+public class CustomSwipeAdapter extends PagerAdapter {
+    private int[] image_resources = {
+            R.drawable.a,
+            R.drawable.b,
+            R.drawable.c,
+            R.drawable.d,
+            R.drawable.e,
+            R.drawable.f,
+            R.drawable.g
+    };
+
+    private Context context;
+    private LayoutInflater layoutInflater;
+    public CustomSwipeAdapter(Context context){
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return image_resources.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return (view==(LinearLayout)object);
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View item_view= layoutInflater.inflate(R.layout.swipe_layout,container,false);
+        ImageView imageView = item_view.findViewById(R.id.image_view);
+        TextView textView = item_view.findViewById(R.id.image_count);
+
+        imageView.setImageResource(image_resources[position]);
+        textView.setText("image: " + position);
+        container.addView(item_view);
+        return item_view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((LinearLayout)object);
+    }
+}
--- app/src/main/java/com/example/ahsan/hciproject/MainActivity.java
+++ app/src/main/java/com/example/ahsan/hciproject/MainActivity.java
@@ -1,13 +1,57 @@
 package com.example.ahsan.hciproject;
 
+        import android.support.v4.view.ViewPager;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.view.MotionEvent;
+        import android.view.ViewParent;
+        import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
+    private ViewFlipper viewFlipper;
+    private float lastX;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewFlipper = findViewById(R.id.viewflipper);
+    }
+
+    public boolean onTouchEvent(MotionEvent touchevent) {
+        switch (touchevent.getAction()) {
+
+            case MotionEvent.ACTION_DOWN: {
+                lastX = touchevent.getX();
+                break;
+            }
+            case MotionEvent.ACTION_UP: {
+                float currentX = touchevent.getX();
+
+                if (lastX < currentX) {
+
+                    if (viewFlipper.getDisplayedChild() == 0)
+                        break;
+
+                    viewFlipper.setInAnimation(this, R.anim.in_from_left);
+                    viewFlipper.setOutAnimation(this, R.anim.out_to_right);
+                    // Show The Previous Screen
+                    viewFlipper.showPrevious();
+                }
+
+                // if right to left swipe on screen
+                if (lastX > currentX) {
+                    if (viewFlipper.getDisplayedChild() == 3)
+                        break;
+
+                    viewFlipper.setInAnimation(this, R.anim.in_from_right);
+                    viewFlipper.setOutAnimation(this, R.anim.out_to_left);
+                    // Show the next Screen
+                    viewFlipper.showNext();
+                }
+                break;
+            }
+        }
+        return false;
     }
 }
